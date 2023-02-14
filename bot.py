303f48dfd66fccfe5b53e3fcbb26256f12177cc1
Rishi Sharma <33762894+rishi-s8@users.noreply.github.com>
2023-02-14 22:48:35 +0100

    Full bot

--- bot.py
+++ bot.py
@@ -1,16 +1,42 @@
+import time
+import datetime
+import pause
 import slack
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
 import os
 
 SLACK_TOKEN = os.environ["SLACK_TOKEN"]
-SIGNING_SECRET = os.environ["SIGNING_SECRET"]
-
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
 
 client = slack.WebClient(token=SLACK_TOKEN)
-client.chat_postMessage(channel='#vegetarian-day',text='Hello')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+weekday = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+possibledays = ["Monday","Tuesday","Thursday"]
+
+veggyday = datetime.datetime.fromisoformat('2023-01-09')
+
+def is_veggie_day(dt:datetime.date):
+    day = weekday[dt.weekday()]
+    if day in possibledays:
+        t_delta = dt - veggyday
+        num_days_since_veggyday = t_delta.days
+        return (day == "Thursday" and (num_days_since_veggyday-17)%21 == 0) or (day == "Monday" and (num_days_since_veggyday)%21 == 0) or (day == "Tuesday" and (num_days_since_veggyday-8)%21 == 0)
+    else:
+        return False
+
+while True:
+    if datetime.datetime.now().hour >= 10:
+        tomorrow_date = datetime.datetime.today() + datetime.timedelta(days=1)
+        wakeupTime = datetime.datetime(tomorrow_date.year, tomorrow_date.month, tomorrow_date.day, 10, 0, 0, 0)
+    else:
+        today_date = datetime.datetime.today()
+        wakeupTime = datetime.datetime(today_date.year, today_date.month, today_date.day, 10, 0, 0, 0)
+    pause.until(wakeupTime)
+
+    today_date = datetime.datetime.today()
+    tomorrow_date = today_date + datetime.timedelta(days=1)
+
+    if is_veggie_day(today_date):
+        client.chat_postMessage(channel='#veg_test',text="Today is Vegetarian Day! :broccoli:")
+    elif is_veggie_day(tomorrow_date):
+        client.chat_postMessage(channel='#veg_test',text="Tomorrow is Vegetarian Day! :avocado:")
+
+
