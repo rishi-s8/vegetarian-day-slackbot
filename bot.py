1216c10ea3f870d4718a39997e432f10178b4125
Rishi Sharma <33762894+rishi-s8@users.noreply.github.com>
2023-02-14 21:18:44 +0100

    correct channel

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
 
 client = slack.WebClient(token=SLACK_TOKEN)
-client.chat_postMessage(channel='#justtest',text='Hello')
+client.chat_postMessage(channel='#vegetarian-day',text='Hello')
 
 if __name__ == "__main__":
     app.run(debug=True)
