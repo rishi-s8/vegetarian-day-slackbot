60e7f0b30ecf274d90081900027744cc5ce4a253
Rishi Sharma <33762894+rishi-s8@users.noreply.github.com>
2023-02-14 20:57:20 +0100

    init

---
+++ bot.py
@@ -0,0 +1,16 @@
+import slack
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+import os
+
+SLACK_TOKEN = os.environ["SLACK_TOKEN"]
+SIGNING_SECRET = os.environ["SIGNING_SECRET"]
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
+
+client = slack.WebClient(token=SLACK_TOKEN)
+client.chat_postMessage(channel='#justtest',text='Hello')
+
+if __name__ == "__main__":
+    app.run(debug=True)
